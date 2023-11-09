acc608d74ced684e69298c12861d36bb274550a2
Igor <Igor@DESKTOP-3EPD54P>
2023-11-09 19:27:38 +0300

    Добавьте файлы проекта.

---
+++ Tumakov_9/BankAccount.cs
@@ -0,0 +1,87 @@
+﻿using System.Security.Policy;
+
+namespace Tumakov_9
+{
+    class BankAccount
+    {
+        public enum Type_bank_account
+        {
+            Текущий,
+            Сберегательный
+        }
+        private static uint next_number_account = 1;
+        private uint number_account;
+        private decimal balance;
+        private Type_bank_account bank_account;
+
+        public uint Number_Account
+        {
+            get { return number_account; }
+        }
+        
+        public decimal Balance_Account
+        {
+            get { return balance; }
+        }
+        
+        public Type_bank_account Type_Bank_Account
+        {
+            get { return bank_account; }
+        }
+        
+        
+        private void Generate_next_number_account()
+        {
+            next_number_account++;
+        }
+        
+        public bool Withdraw(uint withdrawn_money)
+        {
+            if (balance - withdrawn_money > 0)
+            {
+                balance -= withdrawn_money;
+                return true;
+            }
+            return false;
+        }
+        
+        public void Put_Money(uint money)
+        {
+            balance += money;
+        }
+        
+        public bool Transfer_money(BankAccount account, decimal money)
+        {
+            if ((money > 0) && (balance - money > 0))
+            {
+                account.balance += money;
+                balance -= money;
+                return true;
+            }
+            return false;
+        }
+
+        
+        public BankAccount(decimal balance, Type_bank_account bank_account)
+        {
+            number_account = next_number_account;
+            this.balance = balance;
+            this.bank_account = bank_account;
+            Generate_next_number_account();
+        }
+
+        public BankAccount(decimal balance)
+        {
+            number_account = next_number_account;
+            this.balance = balance;
+            Generate_next_number_account();
+        }
+
+        public BankAccount(Type_bank_account bank_account)
+        {
+            number_account = next_number_account;
+            this.bank_account = bank_account;
+            Generate_next_number_account();
+        }
+    }
+}
---
+++ Tumakov_9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumakov_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Упр. 9.1
+            BankAccount client1 = new BankAccount(balance: 30012);
+            Console.WriteLine(client1.Balance_Account);
+            Console.WriteLine(client1.Number_Account);
+            BankAccount client2 = new BankAccount(BankAccount.Type_bank_account.Текущий);
+            Console.WriteLine(client2.Type_Bank_Account);
+            Console.WriteLine(client2.Number_Account);
+            BankAccount client3 = new BankAccount(balance: 1343546, BankAccount.Type_bank_account.Сберегательный);
+            Console.WriteLine(client3.Type_Bank_Account);
+            Console.WriteLine(client3.Balance_Account);
+            Console.WriteLine(client3.Number_Account);
+
+            //Упр. 9.2
+            BankAccount2 client4 = new BankAccount2(balance: 23456, BankAccount2.Type_bank_account.Текущий);
+            client4.Put_Money(1234567);
+            Queue<BankTransaction> transactions = client4.Transactions;
+            BankTransaction transaction = transactions.Dequeue();
+            Console.WriteLine(transaction.DateTime_Transaction);
+
+            //Упр. 9.3
+            BankAccount2 client5 = new BankAccount2(balance: 34356, BankAccount2.Type_bank_account.Сберегательный);
+            client5.Put_Money(32456);
+            client5.Withdraw(3243);
+            client5.Dispose(client5);
+        }
+    }
+}
